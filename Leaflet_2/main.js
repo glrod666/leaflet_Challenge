f6849ef61fe2ae4b3f7fb3fa64758b831235b83d
glrod666 <159737121+glrod666@users.noreply.github.com>
2024-06-20 16:50:25 -0400

    Add files

---
+++ Leaflet_2/main.js
@@ -0,0 +1,8 @@
+javascript
+     // Initialize the map
+     var map = L.map('map').setView([51.505, -0.09], 13);
+
+     // Add a tile layer
+     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+     }).addTo(map);
