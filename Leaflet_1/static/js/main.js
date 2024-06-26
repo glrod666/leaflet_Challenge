341c2c32909f9b69f31cc4bda8a9ba6a1205da2f
glrod666 <159737121+glrod666@users.noreply.github.com>
2024-06-26 05:22:54 -0400

    Update main.js

--- Leaflet_1/static/js/main.js
+++ Leaflet_1/static/js/main.js
@@ -1,31 +1,15 @@
-// Create a map object
+// Initialize the map
 var map = L.map('map').setView([37.7749, -122.4194], 5);
 
-// Add a tile layer to the map
+// Add the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Function to get color based on depth
-function getColor(depth) {
-    return depth > 90 ? '#FF0000' :
-           depth > 70 ? '#FF4500' :
-           depth > 50 ? '#FF8C00' :
-           depth > 30 ? '#FFA500' :
-           depth > 10 ? '#FFD700' :
-                        '#FFFF00';
-}
-
-// Function to get radius based on magnitude
-function getRadius(magnitude) {
-    return magnitude * 4;
-}
-
-// Fetch earthquake data
+// Fetch earthquake data and add to the map
 fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
     .then(response => response.json())
     .then(data => {
-        // Create a GeoJSON layer and add it to the map
         L.geoJSON(data, {
             pointToLayer: function (feature, latlng) {
                 return L.circleMarker(latlng, {
@@ -43,15 +27,29 @@ fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojso
         }).addTo(map);
     });
 
-// Create a legend
+// Function to determine color based on depth
+function getColor(depth) {
+    return depth > 90 ? '#FF0000' :
+           depth > 70 ? '#FF4500' :
+           depth > 50 ? '#FF8C00' :
+           depth > 30 ? '#FFA500' :
+           depth > 10 ? '#FFD700' :
+                        '#FFFF00';
+}
+
+// Function to calculate radius based on magnitude
+function getRadius(magnitude) {
+    return magnitude * 4;
+}
+
+// Add legend to the map
 var legend = L.control({ position: 'bottomright' });
 
 legend.onAdd = function () {
     var div = L.DomUtil.create('div', 'info legend'),
         depths = [0, 10, 30, 50, 70, 90],
         labels = [];
-
-    // loop through depth intervals and generate a label with a colored square for each interval
+        
     for (var i = 0; i < depths.length; i++) {
         div.innerHTML +=
             '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' +
@@ -61,4 +59,4 @@ legend.onAdd = function () {
     return div;
 };
 
-legend.addTo(map);
+legend.addTo(map)
